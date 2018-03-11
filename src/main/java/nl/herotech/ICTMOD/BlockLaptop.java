4c9b474d9b80f7cac8bd6a08f2d3d205875d9735
BuildTools <unconfigured@null.spigotmc.org>
2018-03-11 15:29:14 +0100

    Initial commit

---
+++ src/main/java/nl/herotech/ICTMOD/BlockLaptop.java
@@ -0,0 +1,13 @@
+package nl.herotech.ICTMOD;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockLaptop extends Block {
+
+	protected BlockLaptop(Material material) {
+		super(material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/nl/herotech/ICTMOD/Dram.java
@@ -0,0 +1,7 @@
+package nl.herotech.ICTMOD;
+
+import net.minecraft.item.Item;
+
+public class Dram extends Item {
+
+}
